91731a4ad62830f35b6431442d9b6cb8f7b088f4
Andres <andrespablo994@gmail.com>
2023-10-24 22:08:30 -0300

    la referencia a eliminamodal tenia un guion en lugar de un punto

--- checkout.php
+++ checkout.php
@@ -262,6 +262,37 @@
         <script src="js/scripts.js"></script>
         <!-- Scripts para tienda -->
         <script>
+            let eliminaModal = document.getElementById('eliminaModal')
+            eliminaModal-addEventListener('show.bs.modal', function(event)
+            {
+                let button = event.relatedTarget
+                let id = button.getAttribute('data-bs-id')
+                let buttonElimina = eliminaModal.querySelector('.modal-footer #btn-elimina')
+                buttonElimina.value = id
+            })
+
+            function eliminaProducto()
+            {
+                let botonElimina = document.getElementById('btn-elimina')
+                let id = botonElimina.value
+
+                let url = 'clases/actualizar_carrito.php'
+                let formData = new FormData()
+                formData.append('action', 'eliminar')
+                formData.append('id', id)
+
+                fetch(url, {
+                    method: 'POST',
+                    body: formData,
+                    mode: 'cors'
+                }).then(respose => respose.json()) 
+                .then(data =>  {
+                    if(data.ok){
+                        location.reload()
+                    }
+                })
+            }
+            
             function actualizaCantidad(cantidad, id)
             {
                 let url = './clases/actualizar_carrito.php'
@@ -295,36 +326,7 @@
                 })
             }
 
-            let eliminaModal = document.getElementById('eliminaModal')
-            eliminaModal-addEventListener('show.bs.modal', function(event)
-            {
-                let button = event.relatedTarget
-                let id = button.getAttribute('data-bs-id')
-                let buttonElimina = eliminaModal.querySelector('.modal-footer #btn-elimina')
-                buttonElimina.value = id
-            })
-
-            function eliminaProducto()
-            {
-                let botonElimina = document.getElementById('btn-elimina')
-                let id = botonElimina.value
-
-                let url = 'clases/actualizar_carrito.php'
-                let formData = new FormData()
-                formData.append('action', 'eliminar')
-                formData.append('id', id)
-
-                fetch(url, {
-                    method: 'POST',
-                    body: formData,
-                    mode: 'cors'
-                }).then(respose => respose.json()) 
-                .then(data =>  {
-                    if(data.ok){
-                        location.reload()
-                    }
-                })
-            }
+            
         </script>
         <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
         <!-- * *                               SB Forms JS                               * *-->
