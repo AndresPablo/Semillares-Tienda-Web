f2b9a7394584a4afe08b88e5d9b77366ebab8cc6
Andres <andrespablo994@gmail.com>
2023-10-24 21:19:43 -0300

    movidas funciones de eliminacion al  HTML

--- checkout.php
+++ checkout.php
@@ -261,7 +261,71 @@
         <!-- Core theme JS-->
         <script src="js/scripts.js"></script>
         <!-- Scripts para tienda -->
-        <script src="js/mitienda.js"></script>
+        <script>
+            function actualizaCantidad(cantidad, id)
+            {
+                let url = './clases/actualizar_carrito.php'
+                let formData = new FormData()
+                formData.append('action', 'agregar');
+                formData.append('id', id);
+                formData.append('cantidad', cantidad)
+
+                fetch(url, {
+                    method: 'POST',
+                    body: formData,
+                    mode: 'cors'
+                }).then(respose => respose.json()) 
+                .then(data =>  {
+                    if(data.ok){
+                        // recibimos el subtotal
+                        let divsubtotal = document.getElementById('subtotal_' + id);
+                        divsubtotal.innerHTML = data.sub;
+                        let total = 0.00;
+                        let list = document.getElementsByName("subtotal[]");
+
+                        for(let i= 0; i < list.length; i++)
+                        {
+                            total += parseFloat(list[i].innerHTML.replace(/[$,]/g, ''))
+                        }
+                        total = new Intl.NumberFormat('es-AR', {
+                            minimumIntegerDigits: 2
+                        }).format(total)
+                        document.getElementById('total').innerHTML = '<?php echo MONEDA ?>' + total;
+                    }
+                })
+            }
+
+            let eliminaModal = document.getElementById('eliminaModal')
+            eliminaModal-addEventListener('show.bs.modal', function(event)
+            {
+                let button = event.relatedTarget
+                let id = button.getAttribute('data-bs-id')
+                let buttonElimina = eliminaModal.querySelector('.modal-footer #btn-elimina')
+                buttonElimina.value = id
+            })
+
+            function eliminaProducto()
+            {
+                let botonElimina = document.getElementById('btn-elimina')
+                let id = botonElimina.value
+
+                let url = 'clases/actualizar_carrito.php'
+                let formData = new FormData()
+                formData.append('action', 'eliminar');
+                formData.append('id', id);
+
+                fetch(url, {
+                    method: 'POST',
+                    body: formData,
+                    mode: 'cors'
+                }).then(respose => respose.json()) 
+                .then(data =>  {
+                    if(data.ok){
+                        location.reload()
+                    }
+                })
+            }
+        </script>
         <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
         <!-- * *                               SB Forms JS                               * *-->
         <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
