04ac263d5ca907ec266987740290660bc574f15e
Andres <andrespablo994@gmail.com>
2023-11-20 23:31:46 -0300

    nueva pagina de prueba

---
+++ mercado_prueba.php
@@ -0,0 +1,56 @@
+<?php
+
+// SDK Mercado Pago
+require __DIR__ .  '/vendor/autoload.php'; 
+
+// Configurar SDK
+MercadoPago\SDK::setAccessToken('TU_ACCESS_TOKEN');  
+
+// Crear preferencia
+$preference = new MercadoPago\Preference();
+
+// Agregar item al carrito
+$item = new MercadoPago\Item();
+$item->title = 'Mi Producto';
+$item->quantity = 1;
+$item->unit_price = 150; 
+$preference->items = array($item);
+
+// Guardar preferencia
+$preference->save();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Checkout Mercado Pago</title>
+
+  <!-- SDK Mercado Pago -->
+  <script src="https://sdk.mercadopago.com/js/v2"></script>
+
+</head>
+<body>
+  
+  <div class="cho-container"></div>
+
+  <script>
+    // Inicializar Mercado Pago
+    const mp = new MercadoPago('PUBLIC_KEY');
+
+    // Abrir checkout
+    mp.checkout({
+      preference: {
+        id: '<?php echo $preference->id; ?>'
+      },
+      render: {
+        container: '.cho-container', // Class container
+        label: 'Pagar', // Botón de pago
+      }
+    });
+  </script>
+
+</body>
+</html>
