b5aa2e78aa7c083e6c50f0704e9ce6a76bc9c0e7
Andres <andrespablo994@gmail.com>
2023-12-20 20:04:21 -0300

    quitado el .ar de direccion de  gmail

--- clases/enviar_mail.php
+++ clases/enviar_mail.php
@@ -13,18 +13,18 @@ $mail = new PHPMailer(true);
 
 try {
     //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;  //SMTP::DEBUG_OFF;    //Enable verbose debug output
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;  //SMTP::DEBUG_OFF;  //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'c1602108.ferozo.com';                     //Set the SMTP server to send through
+    $mail->Host       = 'c1602108.ferozo.com';                  //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'no-responder@semillares.com.ar';                     //SMTP username
-    $mail->Password   = '@Nore2023';                               //SMTP password
-    $mail->SMTPSecure = 'ssl';            //Enable implicit TLS encryption
+    $mail->Username   = 'no-responder@semillares.com.ar';       //SMTP username
+    $mail->Password   = '@Nore2023';                            //SMTP password
+    $mail->SMTPSecure = 'ssl';                                  //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
     $mail->setFrom('no-responder@semillares.com.ar', 'Tienda Semillares');
-    $mail->addAddress('andrespablo.mm@gmail.com.ar', 'Contacto');     //Add a recipient
+    $mail->addAddress('andrespablo.mm@gmail.com', 'Contacto');     //Add a recipient
     $mail->addReplyTo('contacto@semillares.com.ar', 'Contacto');    
 
     //Content
