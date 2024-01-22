421b53a77e025c96c5bdd79b2e528a7da135ac2f
Andres <andrespablo994@gmail.com>
2024-01-22 00:03:15 -0300

    Update db_test.php

--- clases/db_test.php
+++ clases/db_test.php
@@ -40,27 +40,31 @@ $datos['recontraseña'] = $recontraseña;
 
 $id =  0;
 
-// estamos trabajando con PDO para los valores, pore eso los ?????
-$sql = $con->prepare ("INSERT INTO clientes (nombres, apellidos, email, telefono, dni, estatus, fecha_alta)
-VALUES (?, ?, ?, ?, ?, 1, NOW())");
-if($sql->execute($datos))
-{
-    $id = $con->lastInsertId();
-}
+try{
+    // estamos trabajando con PDO para los valores, pore eso los ?????
+    $sql = $con->prepare ("INSERT INTO clientes (nombres, apellidos, email, telefono, dni, estatus, fecha_alta)
+    VALUES (?, ?, ?, ?, ?, 1, NOW())");
+    if($sql->execute($datos))
+    {
+        $id = $con->lastInsertId();
+    }
 
- // si es mayor a 0 es porque hay un error y nose registro el cliente
- if($id > 0)
- {
-     $pass_hash = password_hash($contraseña, PASSWORD_DEFAULT);
-     $token = generarToken();
-     if(!registraUsuario([$usuario, $password, $token, $id], $con))
-     {
-         $errors[] = "error al registrar Usuario";
-     }
- }else
- {
-     $errors[] = "error al registrar Cliente";
- }
+    // si es mayor a 0 es porque hay un error y nose registro el cliente
+    if($id > 0)
+    {
+        $pass_hash = password_hash($contraseña, PASSWORD_DEFAULT);
+        $token = generarToken();
+        if(!registraUsuario([$usuario, $password, $token, $id], $con))
+        {
+            $errors[] = "error al registrar Usuario";
+        }
+    }else
+    {
+        $errors[] = "error al registrar Cliente";
+    }
+}catch (PDOException $e) {
+    echo "Error PDO: " . $e->getMessage();
+  }
 
  if(count($errors) == 0)
  {
