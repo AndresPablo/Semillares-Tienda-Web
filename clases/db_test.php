0ad0788941a5e376b57c08c13e828970bbbe2402
Andres <andrespablo994@gmail.com>
2024-01-22 00:43:44 -0300

    db_test.php mas simplificada

--- clases/db_test.php
+++ clases/db_test.php
@@ -8,18 +8,5 @@ require 'clienteFunciones.php';
 $db = new Database();
 $con = $db->conectar();   
 
-
-
-$errors = [];
-
-
-$query = "INSERT INTO prueba (nombre, apellido) VALUES ('pepe', 'flores')";
-$result = $db->query($query);
-
-if(!$result) {
-    echo "Error al insertar datos: " . $db->error;
-  } else {
-    echo "Datos insertados exitosamente"; 
-  }
-
-  $db->close();
+$sql = $con->prepare ("INSERT INTO prueba (nombre, apellido) VALUES ('pepe', 'flores')");
+$sql->execute($datos);
