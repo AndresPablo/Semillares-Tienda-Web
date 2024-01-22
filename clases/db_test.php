30b85cc8b9d6aecaa88101615b86d4f7bc1de7c1
Andres <andrespablo994@gmail.com>
2024-01-22 01:00:28 -0300

    Update db_test.php

--- clases/db_test.php
+++ clases/db_test.php
@@ -9,15 +9,25 @@ $db = new Database();
 $con = $db->conectar();   
 
 
+// Valores a mano : FUNCIONA BIEN, hay que omitir el ID
+//$sql = $con->prepare ("INSERT INTO prueba (nombre, apellido) VALUES ('pepe, 'flores')");
+
+// Valores dinamicos insertando un array
+
 $datos = [];
 
 $nombre = 'Pepe';
 $apellido = 'Flores';
 
 // agregar valores 
-$datos['nombre'] = 'Luis'; 
-$datos['apellido'] = 'Gomez';
+$datos['nombre'] = $nombre; 
+$datos['apellido'] = $apellido;
 
+print_r($datos);
 
-$sql = $con->prepare ("INSERT INTO prueba (nombre, apellido) VALUES (?,?)");
+$sql = $con->prepare ("INSERT INTO prueba (nombre, apellido) VALUES (?, ?)"); 
 $sql->execute($datos);
+
+
+
+
