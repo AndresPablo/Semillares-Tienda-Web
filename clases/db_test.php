1020e0338976fe1dd435dddbab211b14f7023198
Andres <andrespablo994@gmail.com>
2024-01-21 23:42:44 -0300

    Update db_test.php
    con valores nulos para los nulos  y la ID autoincremental

--- clases/db_test.php
+++ clases/db_test.php
@@ -37,7 +37,7 @@ $datos['recontraseña'] = $recontraseña;
 $id =  0;
 
 // estamos trabajando con PDO para los valores, pore eso los ?????
-$sql = $con->prepare ("INSERT INTO clientes (nombres, apellidos, email, telefono, dni, estatus, fecha_alta) VALUES(?,?,?,?,?,1, now())");
+$sql = $con->prepare ("INSERT INTO clientes (nombres, apellidos, email, telefono, dni, estatus, fecha_alta) VALUES(NULL,?,?,?,?,?,1, now(), NULL, NULL)");
 if($sql->execute($datos))
 {
     $id = $con->lastInsertId();
