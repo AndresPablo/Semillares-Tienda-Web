8fabf928bf5247191a7cb0f3619d017e44b40d1b
Andres <andrespablo994@gmail.com>
2024-01-22 00:39:40 -0300

    Simplificado con tabla de prueba

--- clases/db_test.php
+++ clases/db_test.php
@@ -2,89 +2,24 @@
 
 require '../config/config.php';
 require '../config/database.php';
-require 'clienteFunciones.php';
+require '/clienteFunciones.php';
 
 // conexion a base de datos
 $db = new Database();
 $con = $db->conectar();   
-
-// Imprimir valor de la conexión
-echo "<pre>";
-print_r($con); 
-echo "</pre>";
-
-// Verifica que no sea null
-if($con == null) {
-  echo "Error de conexión";
-  exit;
-}
-
-// Verifica que esté conectado 
-if(!$con->getAttribute(PDO::ATTR_SERVER_INFO)) {
-  echo "Error de conexión";
-  exit;
-}
 
 
 
 $errors = [];
 
 
-$nombres = 'pepe';
-$apellidos = 'flores';
-$email = 'correo@servicio.com';
-$telefono = '123345';
-$dni = 01010101;
-$usuario = 'miUser99';
-$contraseña = 'Prueba_198654*';
-$recontraseña = 'Prueba_198654*';
+$query = "INSERT INTO prueba (nombre, apellido) VALUES ('pepe', 'flores')";
+$result = $db->query($query);
 
-$datos = [];
-
-// agregar valores 
-$datos['nombres'] = $nombres; 
-$datos['apellidos'] = $apellidos;
-$datos['email'] = $email; 
-$datos['telefono'] = $telefono;
-$datos['dni'] = $dni; 
-$datos['usuario'] = $usuario;
-$datos['contraseña'] = $contraseña; 
-$datos['recontraseña'] = $recontraseña;
-
-
-$id =  0;
-
-try{
-    // estamos trabajando con PDO para los valores, pore eso los ?????
-    $sql = $con->prepare ("INSERT INTO clientes (nombres, apellidos, email, telefono, dni, estatus, fecha_alta)
-    VALUES (?, ?, ?, ?, ?, 1, NOW())");
-    if($sql->execute($datos))
-    {
-        $id = $con->lastInsertId();
-    }
-
-    // si es mayor a 0 es porque hay un error y nose registro el cliente
-    if($id > 0)
-    {
-        $pass_hash = password_hash($contraseña, PASSWORD_DEFAULT);
-        $token = generarToken();
-        if(!registraUsuario([$usuario, $password, $token, $id], $con))
-        {
-            $errors[] = "error al registrar Usuario";
-        }
-    }else
-    {
-        $errors[] = "error al registrar Cliente";
-        error_log(print_r($datos, true)); // registro en log
-    }
-}catch (PDOException $e) {
-    echo "Error PDO: " . $e->getMessage();
+if(!$result) {
+    echo "Error al insertar datos: " . $db->error;
+  } else {
+    echo "Datos insertados exitosamente"; 
   }
 
- if(count($errors) == 0)
- {
-
- }else
- {
-     print_r($errors);
- }
+  $db->close();
