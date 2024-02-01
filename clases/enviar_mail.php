d086e85c236b2d8657a31c2eb30d24c97c836db9
Andres <andrespablo994@gmail.com>
2024-01-31 21:10:09 -0300

    Apagado debug de mail

--- clases/enviar_mail.php
+++ clases/enviar_mail.php
@@ -13,7 +13,7 @@
 
     try {
         //Server settings
-        $mail->SMTPDebug = SMTP::DEBUG_SERVER;  //SMTP::DEBUG_OFF;  //Enable verbose debug output
+        $mail->SMTPDebug = SMTP::DEBUG_OFF;  //SMTP::DEBUG_SERVER;  //Enable verbose debug output
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host       = 'c1602108.ferozo.com';                  //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
