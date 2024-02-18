e85ee31bf021b13c06ce4166a200b1df15f354fe
Andres <andrespablo994@gmail.com>
2024-02-18 14:04:17 -0300

    Update compras.php

--- compras.php
+++ compras.php
@@ -2,7 +2,7 @@
 
 require 'config/config.php';
 require 'config/database.php';
-require 'config/clienteFunciones.php';
+require 'clases/clienteFunciones.php';
 
 $db = new Database();
 $con = $db->conectar();
@@ -14,3 +14,60 @@ $sqlProd = $con->prepare("SELECT id_transaccion, fecha, status, total FROM compr
 $sqlProd->execute([$idCliente]);
 
 ?>
+<!DOCTYPE html>
+<html lang="es-AR">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Semillares - Dietética</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+        <!-- Custom CSS (personalizado)-->
+        <link href="css/custom.css" rel="stylesheet" />
+        <!-- Fuentes (Montserrat) -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500&display=swap" rel="stylesheet">
+    </head>
+
+
+    <body>
+        
+        <!------- -------------------->
+        <main>
+            <div class="container">
+                <h4>Mis compras</h4>
+                <hr>
+                <?php while($row = $sql->fetch(PDO::FETCH_ASSOC)){?>
+                <div class="card">
+                    <div class="card-header">
+                        <?php echo $row['fecha']; ?>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">     <?php echo $row['id_transaccion']; ?>     </h5>
+                        <p class="card-text">  <?php echo $row['total']; ?>  </p>
+                        <a href="compra_detalle.php" class="btn btn-primary">Detalle</a>
+                    </div>
+                </div>
+                <?php } ?>
+            </div>
+        </main>
+        
+        <!------- -------------------->
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <!-- * *                               SB Forms JS                               * *-->
+        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    </body>
+</html>
