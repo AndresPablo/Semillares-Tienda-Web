3937ab6ae34fe5c17097117d8bb4282789f41d3f
Andres <andrespablo994@gmail.com>
2024-02-20 14:07:47 -0300

    Update completado.php

--- completado.php
+++ completado.php
@@ -36,30 +36,44 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="es">
-
+<html lang="es-AR">
     <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
         <title>Completado</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+        <!-- Custom CSS (personalizado)-->
+        <link href="css/custom.css" rel="stylesheet" />
+        <!-- Fuentes (Montserrat) -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500&display=swap" rel="stylesheet">
     </head>
 
     <body>
-        <h2>
-            Gracias por su compra.
-        </h2>
-        <div>
-            <b>Número de orden: </b> <?php echo $id_transaccion; ?><br>
-            <b>Fecha de compra: </b><?php echo $fecha; ?><br>
-            <b>Total: </b><?php echo MONEDA . number_format($total,2, '.', '.');?><br>
-            <br>
-            <p>Te envaimos un correo electrónico a <b><?php echo $email; ?></b> con el detalle de tu compra</p><br>
-            <?php echo $_SESSION['user_mail']; ?>
-            <a href=""> <button>
-                    Volver a la tienda
-            </button></a>
-            <p>No te llegó? Revisá tu correo no deseado y spam. Si no lo encontrás comunicate con nosotros.</p>
+        <div class="container">
+            <h2>
+                Gracias por su compra.
+            </h2>
+            <div class="">
+                <b>Número de orden: </b> <?php echo $id_transaccion; ?><br>
+                <b>Fecha de compra: </b><?php echo $fecha; ?><br>
+                <b>Total: </b><?php echo MONEDA . number_format($total,2, '.', '.');?><br>
+                <br>
+                <p>Te envaimos un correo electrónico a <b><?php echo $email; ?></b> con el detalle de tu compra</p><br>
+                <?php echo $_SESSION['user_mail']; ?>
+                <a href="index.php"> <button class="btn btn-primary">
+                        Volver a la tienda
+                </button></a>
+                <p>No te llegó? Revisá tu correo no deseado y spam. Si no lo encontrás comunicate con nosotros.</p>
+            </div>
         </div>
     </body>
 
