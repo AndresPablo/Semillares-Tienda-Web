89ec196da40a7fe329b69b853e2dfc58676f334c
Andres <andrespablo994@gmail.com>
2024-02-21 21:02:54 -0300

    Update checkout.php
    ajustado el alto de la pagina para que ocupe toda la verticalidad de pantalla

--- checkout.php
+++ checkout.php
@@ -50,7 +50,7 @@
 
         <!-- Contenido -->
         <main>
-            <div class="container">
+            <div class="container vh-100">
                 <div class="table table-responsive">
                     <table class="table">
                         <thead>
