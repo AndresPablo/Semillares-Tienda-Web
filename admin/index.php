22f75a482268e044a1f05a0fda9ecfea2660868e
Andres <andrespablo994@gmail.com>
2024-02-23 02:20:05 -0300

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -43,6 +43,7 @@
             <div id="layoutAuthentication_content">
                 <main>
                     <div class="container">
+                        <?php echo mostrarMensajes($errors); ?>
                         <div class="row justify-content-center">
                             <div class="col-lg-5">
                                 <div class="card shadow-lg border-0 rounded-lg mt-5">
